38d750f030b2303d64f3ae620f548b376a5cb053
Nicolas Maurice <ricemo1989@msn.com>
2017-12-05 09:38:54 +0100

    feat(pyutils-pep487): implement a patch for pep487 compatibility

---
+++ create_python_project/pyutils/__init__.py
@@ -0,0 +1,15 @@
+"""
+    scripts.pyutils
+    ~~~~~~~~~~~~~~~
+
+    Module base utilities for python
+
+    :copyright: (c) 2017 by Nicolas Maurice, see AUTHORS.rst for more details.
+    :license: BSD, see :ref:`license` for more details.
+"""
+
+from .pep487 import object_with_init_subclass
+
+__all__ = [
+    'object_with_init_subclass',
+]
