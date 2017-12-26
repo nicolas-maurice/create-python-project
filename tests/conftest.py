631f2e906d6f71d067750d2e69069ca08b2f6c9a
Nicolas Maurice Valera <nicolas.maurice.valera@gmail.com>
2017-12-26 16:28:47 +0100

    refactor(tests-conftest): refactor conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,7 +16,7 @@ import pytest
 import semver
 from git import Git
 
-from create_python_project import RepositoryManager
+from create_python_project import RepositoryManager, ProjectManager
 
 DIR_NAME = os.path.dirname(__file__)
 TEST_REPO = os.path.join(DIR_NAME, 'repo')
@@ -33,11 +33,10 @@ def repo_path():
     os.chdir(initial_dir)
 
 
-@pytest.fixture(scope='function')
-def repo(monkeypatch, mocker, repo_path, caplog):
+def _prepare_repo(repo_class, repo_path, mocker, monkeypatch, caplog):
     with caplog.at_level(logging.ERROR):  # Set log level to avoid to overcharge logs with GitPython logs
         # Initialize repository for testing
-        _repo = RepositoryManager.init(path=repo_path)
+        repo = repo_class.init(path=repo_path)
 
         # Monkey patch and Mock _repo.git.push function so we do not need to effectively push to remotes during tests.
         # We cannot patch the git.push function due to GitPython implementation of Git class with __slots__
@@ -47,17 +46,17 @@ def repo(monkeypatch, mocker, repo_path, caplog):
             def push(self, *args, **kwargs):
                 pass
 
-        monkeypatch.setattr(_repo, 'git', value=MockGit(_repo.working_dir))
-        mocker.patch.object(_repo.git, 'push')
+        monkeypatch.setattr(repo, 'git', value=MockGit(repo.working_dir))
+        mocker.patch.object(repo.git, 'push')
 
         # Add files, commits and tags to the repository
         version = '0.0.0'
-        for i, file in enumerate(_repo.untracked_files):
-            _repo.git.add(file)
-            _repo.git.commit(file, '-m', 'add {file}'.format(file=file))
+        for i, file in enumerate(repo.untracked_files):
+            repo.git.add(file)
+            repo.git.commit(file, '-m', 'add {file}'.format(file=file))
             if i % 2 == 0:
-                _repo.create_tag('v{version}'.format(version=version),
-                                 message='Release v{version}'.format(version=version))
+                repo.create_tag('v{version}'.format(version=version),
+                                message='Release v{version}'.format(version=version))
                 if i > 0 and i % 8 == 0:
                     version = semver.bump_major(version)
                 elif i > 0 and i % 4 == 0:
@@ -65,6 +64,32 @@ def repo(monkeypatch, mocker, repo_path, caplog):
                 else:
                     version = semver.bump_patch(version)
 
+    return repo
+
+
+def _finalize_repo(repo, repo_path, initial_commit):
+    # Reset changes performed during tests
+    repo.git.reset('--hard', initial_commit)
+    shutil.rmtree(os.path.join(repo_path, '.git'))
+
+
+@pytest.fixture(scope='function')
+def repo(repo_path, monkeypatch, mocker, caplog):
+    _repo = _prepare_repo(RepositoryManager, repo_path, mocker, monkeypatch, caplog)
+
+    initial_commit = _repo.head.commit
+
     yield _repo
 
-    shutil.rmtree(os.path.join(repo_path, '.git'))
+    _finalize_repo(_repo, repo_path, initial_commit)
+
+
+@pytest.fixture(scope='function')
+def manager(repo_path, monkeypatch, mocker, caplog):
+    _manager = _prepare_repo(ProjectManager, repo_path, mocker, monkeypatch, caplog)
+
+    initial_commit = _manager.head.commit
+
+    yield _manager
+
+    _finalize_repo(_manager, repo_path, initial_commit)
